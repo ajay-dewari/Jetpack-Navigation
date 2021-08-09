32dab397a7fea071d53fd879009dcc97697a143d
ajay.dewari <ajdewari@gmail.com>
2021-08-09 13:29:17 +0530

    loginFragment created with navigation from home screen

--- app/src/main/java/me/ajay/jetpack_navigation/HomeFragment.kt
+++ app/src/main/java/me/ajay/jetpack_navigation/HomeFragment.kt
@@ -1,6 +1,19 @@
 package me.ajay.jetpack_navigation
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.button_login
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button_login.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+    }
 }
---
+++ app/src/main/java/me/ajay/jetpack_navigation/LoginFragment.kt
@@ -0,0 +1,6 @@
+package me.ajay.jetpack_navigation
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment: Fragment(R.layout.fragment_login) {
+}
