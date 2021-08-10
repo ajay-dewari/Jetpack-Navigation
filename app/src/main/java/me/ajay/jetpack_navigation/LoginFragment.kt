f0dc2881271b959d091df615fbb5c13bf41c9b2c
ajay.dewari <ajdewari@gmail.com>
2021-08-10 10:23:34 +0530

    WelcomeFragment added with navigation from loginFragment

--- app/src/main/java/me/ajay/jetpack_navigation/LoginFragment.kt
+++ app/src/main/java/me/ajay/jetpack_navigation/LoginFragment.kt
@@ -1,6 +1,18 @@
 package me.ajay.jetpack_navigation
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_login.button_confirm
 
 class LoginFragment: Fragment(R.layout.fragment_login) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button_confirm.setOnClickListener{
+            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment();
+            findNavController().navigate(action)
+        }
+    }
 }
---
+++ app/src/main/java/me/ajay/jetpack_navigation/WelcomeFragment.kt
@@ -0,0 +1,12 @@
+package me.ajay.jetpack_navigation
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
