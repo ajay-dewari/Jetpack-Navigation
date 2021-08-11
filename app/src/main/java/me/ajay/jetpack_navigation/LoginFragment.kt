f871e33cf55443a6bdb0d53494043598eff55d0e
ajay.dewari <ajdewari@gmail.com>
2021-08-11 21:51:42 +0530

    Safe args added to welcome fragment to get credentials from login screen

--- app/src/main/java/me/ajay/jetpack_navigation/LoginFragment.kt
+++ app/src/main/java/me/ajay/jetpack_navigation/LoginFragment.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_login.button_confirm
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment: Fragment(R.layout.fragment_login) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val userName = edit_text_username.text.toString()
+        val pwd = edit_text_password.text.toString()
         button_confirm.setOnClickListener{
-            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment()
+            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(userName, pwd)
             findNavController().navigate(action)
         }
     }
